31c2be9b796fbb3bf82a588371b49b56fc782960
Bhaanu Teja <bhaanuteja999@gmail.com>
2025-02-15 15:05:15 +0530

    Avg_Of_Three_Numbers.java

---
+++ Avg_Of_Three_Numbers.java
@@ -0,0 +1,11 @@
+public class Avg_Of_Three_Numbers {
+    public static void main(String[] args) {
+        double num1 = 10.5;
+        double num2 = 20.3;
+        double num3 = 30.7;
+
+        double average = (num1 + num2 + num3) / 3;
+
+        System.out.println("The average of the three numbers is: " + average);
+    }
+}
