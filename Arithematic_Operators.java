bb32fdc813c88c4c3f1f820d135c2ba0e7e37cd3
Bhaanu Teja <bhaanuteja999@gmail.com>
2025-02-15 15:06:04 +0530

    Arithematic_Operators.java

---
+++ Arithematic_Operators.java
@@ -0,0 +1,21 @@
+public class Arithematic_Operators {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        int difference = num1 - num2;
+        System.out.println("Difference: " + difference);
+
+        int product = num1 * num2;
+        System.out.println("Product: " + product);
+
+        int quotient = num1 / num2;
+        System.out.println("Quotient: " + quotient);
+
+        int remainder = num1 % num2;
+        System.out.println("Remainder: " + remainder);
+    }
+}
